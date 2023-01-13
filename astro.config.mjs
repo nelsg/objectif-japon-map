987872e20b814ea1a727e675d7e231153f4bdfa2
GONCALVES Nelson (T0025615) <nelson.goncalves@thalesgroup.com>
2023-01-13 18:02:13 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nelsg.github.io/objectif-japon-map',
-    base: '/objectif-japon-map',
+    // site: 'https://nelsg.github.io/objectif-japon-map',
+    // base: '/objectif-japon-map',
 });
